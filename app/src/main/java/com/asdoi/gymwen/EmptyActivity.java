cf98b8264316bdef3c9b5695211a5aab410ba4d5
asdoi <muell-4@t-online.de>
2019-11-23 00:00:00 +0000

    Fixed widget, calling updateWidget in update Method :D

---
+++ app/src/main/java/com/asdoi/gymwen/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.asdoi.gymwen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
