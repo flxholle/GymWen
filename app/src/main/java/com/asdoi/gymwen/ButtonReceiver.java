909f6145745e17c8a3eb59c5d227b5c6d12c3499
asdoi <Muell-4@t-online.de>
2019-11-27 00:00:00 +0000

    Added Notification-Dismiss Button

---
+++ app/src/main/java/com/asdoi/gymwen/ButtonReceiver.java
@@ -0,0 +1,18 @@
+package com.asdoi.gymwen;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ButtonReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int notificationId = intent.getIntExtra("notificationId", 0);
+
+        // if you want cancel notification
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(notificationId);
+    }
+}
