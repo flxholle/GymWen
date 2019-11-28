598ce1a47ff8e2a03ca7dc59e7361b3603866b39
asdoi <muell-4@t-online.de>
2019-11-28 00:00:00 +0000

    Added Notification Settings

--- app/src/main/java/com/asdoi/gymwen/main/SettingsActivity.java
+++ app/src/main/java/com/asdoi/gymwen/main/SettingsActivity.java
@@ -3,17 +3,18 @@ package com.asdoi.gymwen.main;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.asdoi.gymwen.ActivityFeatures;
+import com.asdoi.gymwen.ApplicationFeatures;
 import com.asdoi.gymwen.R;
 import com.asdoi.gymwen.vertretungsplanInternal.VertretungsPlan;
 
-public class SettingsActivity extends ActivityFeatures implements View.OnClickListener {
+public class SettingsActivity extends ActivityFeatures {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +47,15 @@ public class SettingsActivity extends ActivityFeatures implements View.OnClickLi
         finish();
     }
 
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
-
-    @Override
-    public void onClick(View v){
-//        if(v.getId() == R.id.settings_choice){
-//            initSettings();
-//            Intent i = new Intent(this, ChoiceActivity.class);
-//            startActivity(i);
-//            return;
-//        }
-    }
-
     public void setSettings() {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean oberstufe = sharedPref.getBoolean("oberstufe", false);
         String courses = sharedPref.getString("courses", "");
         if (courses.trim().isEmpty()) {
             Intent i = new Intent(this, ChoiceActivity.class);
             startActivity(i);
             return;
         }
-        oberstufe = courses.split("#").length > 1;
+        boolean oberstufe = courses.split("#").length > 1;
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putBoolean("oberstufe", oberstufe);
         editor.commit();
@@ -86,5 +68,25 @@ public class SettingsActivity extends ActivityFeatures implements View.OnClickLi
 
         System.out.println("settings: " + oberstufe + courses);
 //        System.out.println("login " + username + password + "real " + VertretungsPlan.strUserId + VertretungsPlan.strPasword);
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            setNotif();
+            Preference myPref = findPreference("showNotification");
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    setNotif();
+                    return true;
+                }
+            });
+        }
+
+        private void setNotif() {
+            boolean showNotif = PreferenceManager.getDefaultSharedPreferences(ApplicationFeatures.getContext()).getBoolean("showNotification", false);
+            findPreference("alwaysNotification").setEnabled(showNotif);
+        }
     }
 }
