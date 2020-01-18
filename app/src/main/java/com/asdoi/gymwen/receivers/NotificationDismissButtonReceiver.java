8c68a10f076967b634bfd99a46daaa61df688daa
asdoi <asdoi@t-online.de>
2020-01-18 00:00:00 +0000

    Changed Notifications

--- app/src/main/java/com/asdoi/gymwen/receivers/NotificationDismissButtonReceiver.java
+++ app/src/main/java/com/asdoi/gymwen/receivers/NotificationDismissButtonReceiver.java
@@ -5,11 +5,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.asdoi.gymwen.ApplicationFeatures;
+
 public class NotificationDismissButtonReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        int notificationId = intent.getIntExtra("notificationId", 0);
+        int notificationId = intent.getIntExtra("EXTRA_NOTIFICATION_ID", ApplicationFeatures.NOTIFICATION_ID);
 
         // if you want cancel notification
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
