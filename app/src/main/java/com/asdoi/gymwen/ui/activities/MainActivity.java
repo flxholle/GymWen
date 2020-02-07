d1c055f87b1432f760676768a51a0179b51962d5
asdoi <asdoi@t-online.de>
2020-02-07 00:00:03 +0000

    fixed spinner colorization

--- app/src/main/java/com/asdoi/gymwen/ui/activities/MainActivity.java
+++ app/src/main/java/com/asdoi/gymwen/ui/activities/MainActivity.java
@@ -195,7 +195,7 @@ public class MainActivity extends ActivityFeatures implements NavigationView.OnN
     }
 
     public void setupColors() {
-        findViewById(R.id.main_profile_spinner).setBackgroundColor(ApplicationFeatures.getPrimaryColor(this));
+        findViewById(R.id.main_spinner_relative).setBackgroundColor(ApplicationFeatures.getPrimaryColor(this));
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setBackgroundColor(ApplicationFeatures.getPrimaryColor(this));
         tabs.setSelectedTabIndicatorColor(ApplicationFeatures.getAccentColor(this));
