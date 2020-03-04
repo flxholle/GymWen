a4ab1db107c4c7726f010cc35bc52e11f415b60e
asdoi <asdoi@t-online.de>
2020-03-04 00:00:00 +0000

    added parts of hide menu entries

---
+++ app/src/main/java/com/asdoi/gymwen/ui/fragments/SettingsFragmentHideMenuItems.java
@@ -0,0 +1,52 @@
+package com.asdoi.gymwen.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
+
+import com.asdoi.gymwen.R;
+
+public class SettingsFragmentHideMenuItems extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences_hide_menu_items, rootKey);
+
+        setFilteredUnfiltered();
+        Preference myPref = findPreference("menu_filtered");
+        myPref.setOnPreferenceClickListener((Preference preference) -> {
+            setFilteredUnfiltered();
+            return true;
+        });
+
+        myPref = findPreference("menu_unfiltered");
+        myPref.setOnPreferenceClickListener((Preference preference) -> {
+            setFilteredUnfiltered();
+            return true;
+        });
+
+        myPref = findPreference("show_sections");
+        myPref.setOnPreferenceClickListener((Preference preference) -> {
+            setBoth();
+            return true;
+        });
+    }
+
+    private void setFilteredUnfiltered() {
+        SwitchPreference switchPreference = findPreference("show_sections");
+        boolean showSum = !((SwitchPreference) findPreference("menu_filtered")).isChecked() && !((SwitchPreference) findPreference("menu_unfiltered")).isChecked();
+        switchPreference.setChecked(!showSum);
+    }
+
+    private void setBoth() {
+        SwitchPreference switchPreference = findPreference("show_sections");
+        boolean showBoth = switchPreference.isChecked();
+        findPreference("menu_filtered").setEnabled(showBoth);
+        findPreference("menu_unfiltered").setEnabled(showBoth);
+
+        boolean showSum = !((SwitchPreference) findPreference("menu_filtered")).isChecked() && !((SwitchPreference) findPreference("menu_unfiltered")).isChecked();
+        if (showSum && showBoth)
+            ((SwitchPreference) findPreference("menu_filtered")).setChecked(true);
+    }
+}
