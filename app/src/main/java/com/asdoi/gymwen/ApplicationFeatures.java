f71137d6b77b96054f78257474239bd5d5113fb2
asdoi <asdoi@t-online.de>
2020-03-05 00:00:04 +0000

    replaced ImageViews with PhotoViews

--- app/src/main/java/com/asdoi/gymwen/ApplicationFeatures.java
+++ app/src/main/java/com/asdoi/gymwen/ApplicationFeatures.java
@@ -215,7 +215,6 @@ public class ApplicationFeatures extends MultiDexApplication {
                     InputStream in = new java.net.URL(urldisplay).openStream();
                     mIcon11 = BitmapFactory.decodeStream(in);
                 } catch (Exception e) {
-//                Log.e("Error", e.getMessage());
                     e.printStackTrace();
                 }
                 return mIcon11;
